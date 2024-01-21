66c28a97dd4f5c836ebe347fd960c5c8b30618df
Amirhossein <4mirh03einhashemi@gmail.com>
2024-01-21 12:18:10 +0330

    create split form ui

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ export default function App() {
         <FormAddFriend />
         <Button>Add friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -82,6 +84,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍‍♂️ Your expense</label>
+      <input type="text" />
+
+      <label>👩🏾‍🤝‍🧑🏼 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill ?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
